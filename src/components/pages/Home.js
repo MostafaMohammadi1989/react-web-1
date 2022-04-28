992e88d4c3836730b2eeb4f4b9ee04870b2a43de
MostafaMohammadi1989 <Mostafa.mohammadi.1989.10.26@gmail.com>
2022-04-28 22:55:06 +0430

    complete web

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,15 @@
-import "../../App.css";
-import HeroSection from "../HeroSection";
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
+      <Footer />
     </>
   );
 }
